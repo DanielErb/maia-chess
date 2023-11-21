d2d103e354fe04a723cef53e1fc33bbcc1b6d9f1
Daniel Erbesfeld <102467176+DanielErb@users.noreply.github.com>
2023-11-21 14:22:26 +0200

    Add files via upload

---
+++ move_prediction/replication-make-leela-files-4.sh
@@ -0,0 +1,45 @@
+
+#!/bin/bash
+
+cd "$(dirname "$0")"
+echo "Current working directory: $(pwd)"
+
+
+process_elo() {
+    elo=$1
+    mkdir -p "../data/elo_ranges/${elo}"
+    outputtest="../data/elo_ranges/${elo}/test"
+    outputtrain="../data/elo_ranges/${elo}/train"
+    
+    mkdir -p $outputtest
+    mkdir -p $outputtrain
+
+    for te in "../data/pgns_ranged_training/${elo}"/*; do
+        fname="$(basename -- $te)"
+        echo "${elo}-${fname}"
+        current_dir=$(pwd)
+        cd $outputtrain
+        mkdir $fname
+        cd $fname
+        trainingdata-tool -v -files-per-dir 5000 ${current_dir}/${te}
+        cd "$current_dir"
+    done
+
+    for te in "../data/pgns_ranged_testing/${elo}"/{1..3}.pgn; do
+        fname="$(basename -- $te)"
+        echo "${elo}-${fname}"
+        current_dir=$(pwd)
+        cd $outputtest
+        mkdir $fname
+        cd $fname
+        trainingdata-tool -v -files-per-dir 5000 ${current_dir}/${te}
+        cd "$current_dir"
+    done
+
+}
+
+# Using parallel for post-loop processing
+export -f process_elo
+parallel process_elo ::: {1800..1900..100}
+
+echo "Current working directory: $(pwd)"
